a85cb43a37fb4c40c743a33249e20bc4d8e6f87f
matthew paquette <matthewpa14@gmail.com>
2025-03-25 12:02:20 -0400

    Retry

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from config import Config
+#from config import Config
 from dotenv import load_dotenv
 import os
 from datetime import timedelta
